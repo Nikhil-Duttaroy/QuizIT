02f88975e8f5998e48f7fa79fd99d298d86d6bc6
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2020-09-14 11:53:18 +0530

    revert

--- js/script.js
+++ js/script.js
@@ -98,18 +98,6 @@ R.addEventListener('mouseover',() => {
 })
 
 
-//login and signup
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
-
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
-
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
 
 
 
