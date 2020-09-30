935c66371b3d1bef1d5a490f9baf77d74589c05a
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2020-09-30 10:08:26 +0530

    marker and circle added on map

--- contact.php
+++ contact.php
@@ -5,9 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
     <link rel="stylesheet" href="css/index.css">
-    <link rel = "stylesheet" href = "http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
-    <script src = "http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
-   <style>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+   crossorigin=""/>
+   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+   crossorigin=""></script>
+
+
+   
+<style>
    #mapid { 
      width: 300px; height: 300px;
       margin: auto;
--- js/contactmap.js
+++ js/contactmap.js
@@ -1,43 +1,29 @@
+var mymap = L.map('mapid').setView([18.990262, 73.127692], 13);
 
-console.log("hello");
-var mymap = L.map('mapid').setView([18.990292, 73.127670], 16);
+var layer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+ );
+mymap.addLayer(layer);
 
+var marker = L.marker([18.990262, 73.127692]).addTo(mymap);
 
-var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-         
-         // Adding layer to the map
-         mymap.addLayer(layer);
+var circle = L.circle([18.988443, 73.128613], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 50
+}).addTo(mymap);
 
-var marker = L.marker([18.990292, 73.127670]).addTo(mymap);
-
-marker.bindPopup("<b>Pillai College of Engineering").openPopup();
-var popup = L.popup();
-
-// var circle = L.circle({lat : 18.990292,lng : 73.127670}, {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 500
-// }).addTo(mymap);
-// var circle = L.circle([18.9894, 73.1175], {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 100
-// }).addTo(mymap);
-
-var polygon = L.polygon([
-    [18.9894, 73.1175],
-    [18.9880, 73.1170],
-    [18.9886, 73.1180]
-]).addTo(mymap);
+marker.bindPopup("Pillai College of Engineering").openPopup();
+circle.bindPopup("Pillai's Hostel");
 
 
-
+var popup = L.popup();  
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
-        .setContent("Clicked at location " + e.latlng.toString())
+        .setContent("Lat , Long =  " + e.latlng.toString())
         .openOn(mymap);
 }
+
 mymap.on('click', onMapClick);
+
