aa11fad27c2451705669214c350efdb12e3bc93b
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2020-09-30 11:40:32 +0530

    contact page styled

--- contact.php
+++ contact.php
@@ -5,15 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/contact.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
    crossorigin=""/>
    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-   crossorigin=""></script>
-
-
-   
+   crossorigin=""></script> 
 <style>
    #mapid { 
      width: 300px; height: 300px;
@@ -24,30 +22,36 @@
 </head>
 <body>
 <?php include 'nav.php' ?>
-<form
-        id="contact-form"
-        action="POST"
-        name="ContactForm"      
-      >
-        <i class="fas fa-times fa-1x close"></i>
-        <h2 style="text-align: center">Get in Touch</h2>
+  <div class="formMapContain">
+      <div class="modal">
+        <form
+                id="contact-form"
+                action="POST"
+                name="ContactForm"      
+              >
+                <i class="fas fa-times fa-1x close"></i>
+                <h2 style="text-align: center">Get in Touch</h2>
 
-        <label>Name</label>
-        <input required class="input-field" type="text" name="name" />
+                <label>Name</label>
+                <input required class="input-field" type="text" name="name" />
 
-        <label>Subject</label>
-        <input required class="input-field" type="text" name="subject" />
+                <label>Subject</label>
+                <input required class="input-field" type="text" name="subject" />
 
-        <label>Email</label>
-        <input required class="input-field" type="text" name="email" />
+                <label>Email</label>
+                <input required class="input-field" type="text" name="email" />
 
-        <label>Message</label>
-        <textarea required class="input-field" name="message"></textarea>       
-        <input id="submit-btn" type="submit" value="Send" />
-      </form>
+                <label>Message</label>
+                <textarea required class="input-field" name="message"></textarea>       
+                <input id="submit-btn" type="submit" value="Send" />
+        </form>
+      </div>  
+      <div class="mapContain">
       <div id="mapid">
         
       </div>
+      </div>
+  </div>     
 </body>
 <script src="js/contactmap.js"></script>
 </html>
